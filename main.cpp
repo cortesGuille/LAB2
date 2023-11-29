f14dbd3da48af9ff98687653aca44ff39702b95c
cortesGuille <146741898+cortesGuille@users.noreply.github.com>
2023-11-29 16:45:29 +0000

    guardarPartida

--- main.cpp
+++ main.cpp
@@ -2,15 +2,18 @@
 #include <limits>
 #include <vector>
 #include <cstring> 
+#include <fstream>
+#include <sstream>
 
 using namespace std;
-
+//Valores constantes para evitar problemas de compilación
 const int FILAS = 6;
 const int COLUMNAS = 7;
 const int JUGADOR_MAX = 1;
 const int JUGADOR_MIN = 2;
 const int INF = 1000000000;
 
+//Estructura para el Nodo y poder realizar el arbol
 struct Nodo{
 
 int tablero[FILAS][COLUMNAS];
@@ -24,62 +27,195 @@ bool hayGanadorEnFilas(int tablero[FILAS][COLUMNAS]);
 bool hayGanadorEnColumnas(int tablero[FILAS][COLUMNAS]);
 bool hayGanadorEnDiagonales(int tablero[FILAS][COLUMNAS]);
 bool movimientoValido(int tablero[FILAS][COLUMNAS], int columna);
-bool movimientoValido(int tablero[FILAS][COLUMNAS], int columna);
 void realizarMovimiento(int tablero[FILAS][COLUMNAS], int columna, int jugador);
 bool esTerminal(int tablero[FILAS][COLUMNAS]);
 int minimax(Nodo* nodo, int profundidad,bool esMaximo, int alpha, int beta);
 Nodo* construirArbol(int tablero[FILAS][COLUMNAS], int profundidad, int jugador, int columna);
 int evaluarTablero(int tablero[FILAS][COLUMNAS]);
-int evaluarVentana(int c1, int c2, int c3, int c4);
+int evaluarPosicion(int c1, int c2, int c3, int c4);
 int obtenerMejorMovimientoIA(int tablero[FILAS][COLUMNAS], int profundidad, Nodo* raiz);
+void copyBoard(int tablero[FILAS][COLUMNAS], int nuevoTablero[FILAS][COLUMNAS]);
+bool verificarTablero(int tablero[FILAS][COLUMNAS]);
+void cargarPartida(const std::string& nombreArchivo, int matriz[FILAS][COLUMNAS]);
+void guardarPartida(string nombreArchivo, int tablero[FILAS][COLUMNAS]);
+
+
 
 
 int main(){
 
     int tablero[FILAS][COLUMNAS] = {0}; // Inicializa el tablero según tu juego
-    int profundidadMaxima = 3; // Ajusta la profundidad máxima según tus necesidades
+    int tableroGuardado[FILAS][COLUMNAS] = {0}; //Tablero auxiliar para guardar la partida
+    int profundidadMaxima; // Profundidad máxima del árbol
+    
+    //VERIFICAR SI EXITE PARTIDA GUARDADA OSEA QUE EL TABLERO NO ESTE LLENO DE 0
+    cargarPartida("partida.txt",tableroGuardado);
+    if (!verificarTablero(tableroGuardado)) {
+        int op;
+        while(op != 1 && op != 2){
+        cout<<"Existe una partida guardada"<<endl;
+        cout<<"Deseas Continuar? 1. Si 2. No" <<endl;
+        
+        cin>>op;
+        if(op == 1){
+            cargarPartida("partida.txt", tablero);
+            cout<<"Partida Cargada"<<endl;
+            imprimirTablero(tablero);
+        }else if (op==2){
+            cout<<"Se crea Nueva Partida"<<endl;
+        }else{
+            cout<<"Numero no valido"<<endl;
+        } 
+        }      
+    }
+    //Ingresar la dificultad
+    int op;
+    bool finBucle = false;
+    while (!finBucle) {
+        cout << "Ingresa el tipo de Dificultad al Juego" << endl;
+        cout << "1. Facil" << endl;
+        cout << "2. Normal" << endl;
+        cout << "3. Dificil" << endl;
+        cin >> op;
+
+        switch (op) {
+            case 1:
+                profundidadMaxima = 3;
+                finBucle = true;
+                break;
+            case 2:
+                profundidadMaxima = 5;
+                finBucle= true;
+                break;
+            case 3:
+                profundidadMaxima = 7;
+                finBucle = true;
+                break;
+            default:
+                cout << "Numero no valido" << endl;
+                break;
+        }
+    }
 
     while (!esTerminal(tablero)) {
-        // Turno del jugador
+        
+        //Movimientos del jugador
         int columnaJugador;
         cout << "Turno del jugador. Elige una columna (0-6): ";
         cin >> columnaJugador;
-
         while (!movimientoValido(tablero, columnaJugador)) {
-        cout << "Movimiento no válido. Elige otra columna: ";
+        cout << "Movimiento no válido, elige otra columna: ";
         cin >> columnaJugador;
         }
-
         realizarMovimiento(tablero, columnaJugador, JUGADOR_MAX);
         imprimirTablero(tablero);
 
-        // Turno de la IA
+
+        //Movimientos de la IA
         Nodo* raiz = construirArbol(tablero, profundidadMaxima, JUGADOR_MIN, columnaJugador);
         int columnaIA = obtenerMejorMovimientoIA(tablero, profundidadMaxima,raiz);
         realizarMovimiento(tablero, columnaIA, JUGADOR_MIN);
         cout << "La IA elige la columna: " << columnaIA <<endl;
         imprimirTablero(tablero);
+    //Esta parte hacela una funcion 
+        int op=0;
+        //Guardar la partida si el jugador desea
+        while(op!=1 && op!=2){
+        cout<<"Quieres terminar la partida?"<<endl;
+        cout<<"1. Si 2. No"<<endl;
+        cin>>op;
+        if(op==1){
+            guardarPartida("partida.txt", tablero);
+            break;
+        }else if(op==2){
+            cout<<"Se Continua el juego"<<endl;
+        }
     }
-
-
-
-
-
-
-
+      
+    }
 
 return 0;
 }
+//VEIRIFCA SI EL TABLERO ESTA LLENO DE 0
+bool verificarTablero(int tablero[FILAS][COLUMNAS]) {
+    for (int i = 0; i < FILAS; ++i) {
+        for (int j = 0; j < COLUMNAS; ++j) {
+            if (tablero[i][j] != 0) {
+                return true;
+            }
+        }
+       
+    }
+    cout<<"Tablero vacio"<<endl;
+    return false;
+}    
+//CARGA EL ARCHIVO CON LA PARTIDA GUARDADA en el txt
+void cargarPartida(const std::string& nombreArchivo, int matriz[FILAS][COLUMNAS]) {
+    ifstream archivo(nombreArchivo);
 
+    // Verifica si el archivo se abrió correctamente
+    if (!archivo.is_open()) {
+        cerr << "Error al abrir el archivo." << std::endl;
+        return;
+    }
+
+    int filas = 0;
+
+    // Lee cada línea del archivo
+    std::string linea;
+    while (std::getline(archivo, linea) && filas < FILAS) {
+        stringstream ss(linea);
+        string elemento;
+        int columnas = 0;
+
+        // Divide la línea en elementos usando la coma como delimitador
+        while (getline(ss, elemento, ',') && columnas < COLUMNAS) {
+            // Convierte cada elemento a entero y lo asigna a la matriz
+            matriz[filas][columnas] = stoi(elemento);
+            ++columnas;
+        }
+
+        ++filas;
+    }
+
+    // Cierra el archivo
+    archivo.close();
+}
+void guardarPartida(string nombreArchivo, int tablero[FILAS][COLUMNAS]) {
+    ofstream archivo(nombreArchivo);
+    if(archivo.is_open()){
+        for (int i = 0; i < FILAS; ++i) {
+            for (int j = 0; j < COLUMNAS; ++j) {
+                archivo << tablero[i][j];
+                if(j<COLUMNAS-1){
+                    archivo << ",";
+                }
+            }
+            archivo << endl;
+        }
+        archivo.close();
+    }
+    else{
+        cout<<"No se pudo abrir el archivo"<<endl;
+    }
+}
+
+void copyBoard(int tablero[FILAS][COLUMNAS], int nuevoTablero[FILAS][COLUMNAS]){
+    for (int i = 0; i < FILAS; ++i) {
+        for (int j = 0; j < COLUMNAS; ++j) {
+            nuevoTablero[i][j] = tablero[i][j];
+        }
+    }
+}
 
 void imprimirTablero(int tablero[FILAS][COLUMNAS]) {
     for (int fila = 0; fila < FILAS; ++fila) {
         for (int columna = 0; columna < COLUMNAS; ++columna) {
-            std::cout << tablero[fila][columna] << " ";
+           cout << tablero[fila][columna] << " ";
         }
-        std::cout << std::endl;
+        cout << endl;
     }
-    std::cout << std::endl;
+    cout << endl;
 }
 
 bool hayGanadorEnFilas(int tablero[FILAS][COLUMNAS]) {
@@ -172,6 +308,7 @@ void realizarMovimiento(int tablero[FILAS][COLUMNAS], int columna, int jugador) 
         tablero[fila][columna] = jugador;
     }
 }
+
 bool esTerminal(int tablero[FILAS][COLUMNAS]) {
     // Verificar si hay un ganador en filas, columnas o diagonales
     if (hayGanadorEnFilas(tablero) || hayGanadorEnColumnas(tablero) || hayGanadorEnDiagonales(tablero)) {
@@ -189,6 +326,7 @@ bool esTerminal(int tablero[FILAS][COLUMNAS]) {
 
     return true;  // El tablero está lleno, el juego es un empate
 }
+
 int minimax(Nodo* nodo, int profundidad,bool esMaximo, int alpha, int beta){
 if(profundidad==0 || esTerminal(nodo->tablero)){
     return evaluarTablero(nodo->tablero);
@@ -219,9 +357,10 @@ return minEv;
 }
 
 }
+
 Nodo* construirArbol(int tablero[FILAS][COLUMNAS], int profundidad, int jugador, int columna) {
     Nodo* nodo = new Nodo();
-    memcpy(nodo->tablero, tablero, sizeof(tablero));
+    copyBoard(tablero, nodo->tablero);
     nodo->columna = columna;
     nodo->valor = 0;
 
@@ -229,7 +368,7 @@ Nodo* construirArbol(int tablero[FILAS][COLUMNAS], int profundidad, int jugador,
         for (int i = 0; i < COLUMNAS; ++i) {
             if (movimientoValido(tablero, i)) {
                 int nuevoTablero[FILAS][COLUMNAS];
-                memcpy(nuevoTablero, tablero, sizeof(tablero));
+                copyBoard(tablero, nuevoTablero);
                 realizarMovimiento(nuevoTablero, i, jugador);
 
                 Nodo* hijo = construirArbol(nuevoTablero, profundidad - 1, 3 - jugador, i);
@@ -240,43 +379,41 @@ Nodo* construirArbol(int tablero[FILAS][COLUMNAS], int profundidad, int jugador,
 
     return nodo;
 }
+
 int evaluarTablero(int tablero[FILAS][COLUMNAS]) {
     // Esta es una función de evaluación muy simple.
     // Puedes personalizarla según las reglas específicas de tu juego.
-
     int puntaje = 0;
-
     // Evaluar las filas
     for (int i = 0; i < FILAS; ++i) {
         for (int j = 0; j < COLUMNAS - 3; ++j) {
-            puntaje += evaluarVentana(tablero[i][j], tablero[i][j + 1], tablero[i][j + 2], tablero[i][j + 3]);
+            puntaje += evaluarPosicion(tablero[i][j], tablero[i][j + 1], tablero[i][j + 2], tablero[i][j + 3]);
         }
     }
-
     // Evaluar las columnas
     for (int i = 0; i < FILAS - 3; ++i) {
         for (int j = 0; j < COLUMNAS; ++j) {
-            puntaje += evaluarVentana(tablero[i][j], tablero[i + 1][j], tablero[i + 2][j], tablero[i + 3][j]);
+            puntaje += evaluarPosicion(tablero[i][j], tablero[i + 1][j], tablero[i + 2][j], tablero[i + 3][j]);
         }
     }
 
     // Evaluar diagonales ascendentes
     for (int i = 3; i < FILAS; ++i) {
         for (int j = 0; j < COLUMNAS - 3; ++j) {
-            puntaje += evaluarVentana(tablero[i][j], tablero[i - 1][j + 1], tablero[i - 2][j + 2], tablero[i - 3][j + 3]);
+            puntaje += evaluarPosicion(tablero[i][j], tablero[i - 1][j + 1], tablero[i - 2][j + 2], tablero[i - 3][j + 3]);
         }
     }
 
     // Evaluar diagonales descendentes
     for (int i = 3; i < FILAS; ++i) {
         for (int j = 3; j < COLUMNAS; ++j) {
-            puntaje += evaluarVentana(tablero[i][j], tablero[i - 1][j - 1], tablero[i - 2][j - 2], tablero[i - 3][j - 3]);
+            puntaje += evaluarPosicion(tablero[i][j], tablero[i - 1][j - 1], tablero[i - 2][j - 2], tablero[i - 3][j - 3]);
         }
     }
-
     return puntaje;
 }
-int evaluarVentana(int c1, int c2, int c3, int c4) {
+
+int evaluarPosicion(int c1, int c2, int c3, int c4) {
     // Esta función asigna un puntaje a una ventana de 4 fichas consecutivas.
 
     int puntaje = 0;
@@ -308,6 +445,7 @@ int evaluarVentana(int c1, int c2, int c3, int c4) {
 
     return puntaje;
 }
+
 int obtenerMejorMovimientoIA(int tablero[FILAS][COLUMNAS], int profundidad, Nodo* raiz) {
     int movimientosValidos[COLUMNAS];
     int numMovimientos = 0;
@@ -324,7 +462,7 @@ int obtenerMejorMovimientoIA(int tablero[FILAS][COLUMNAS], int profundidad, Nodo
     for (int i = 0; i < numMovimientos; ++i) {
         int nuevaColumna = movimientosValidos[i];
         int nuevoTablero[FILAS][COLUMNAS];
-        memcpy(nuevoTablero, tablero, sizeof(tablero));
+        copyBoard(tablero, nuevoTablero);
         realizarMovimiento(nuevoTablero, nuevaColumna, JUGADOR_MAX);
 
         // Busca el nodo hijo correspondiente a la columna actual
